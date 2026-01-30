48c5871ca72e31e414588666044d57486cd687d0
@jota_coder <142507402+ojaimevitor@users.noreply.github.com>
2026-01-30 18:44:27 -0300

    Switch router from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
-//import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
