2bb53e5256fa24d1c2c6cf5ec8e6006f224b5ddf
@jota_coder <142507402+ojaimevitor@users.noreply.github.com>
2026-01-30 19:47:11 -0300

    Update runtime.js

--- src/lib/runtime.js
+++ src/lib/runtime.js
@@ -1,2 +1,3 @@
-// src/lib/runtime.js
-export const IS_DEMO = import.meta.env.MODE === "production" && location.host.includes("github.io");
+export const IS_BASE44 = window.location.hostname.endsWith("base44.app");
+export const IS_GH_PAGES = window.location.hostname.endsWith("github.io");
+export const IS_STANDALONE = !IS_BASE44;
